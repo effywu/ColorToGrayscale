ec0bb4b4c00f3875d3687e51518d39f74eda0f76
Effy Wu <effywu1993@gmail.com>
2016-05-18 17:41:15 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FirstWindowsProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(225, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pb
+            // 
+            this.pb.Location = new System.Drawing.Point(42, 78);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(463, 194);
+            this.pb.TabIndex = 4;
+            this.pb.TabStop = false;
+            this.pb.Click += new System.EventHandler(this.pb_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 360);
+            this.Controls.Add(this.pb);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Random";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace FirstWindowsProject
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        string textholder;
+        System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+        Bitmap greyimg;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static Bitmap ToGrayscale(Bitmap original)
+        {
+            Bitmap greybitmap = new Bitmap(original.Width, original.Height);
+            Graphics gr = Graphics.FromImage(greybitmap);
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][]
+                {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1}
+                });
+            ImageAttributes attributes = new ImageAttributes();
+            attributes.SetColorMatrix(colorMatrix);
+            gr.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height), 0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+            gr.Dispose();
+            return greybitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect=false;
+            openFileDialog1.Filter="jpg files|*.jpg";
+            openFileDialog1.FilterIndex=0;
+            openFileDialog1.DefaultExt=".jpg";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap img = new Bitmap(openFileDialog1.FileName);
+                greyimg = ToGrayscale(img);
+                img.Dispose();
+                int ogWidth = greyimg.Width;
+                int ogHeight = greyimg.Height;
+
+                float ratioX = (float)475 / (float)ogWidth;
+                float ratioY = (float)192 / (float)ogHeight;
+                float ratio = Math.Min(ratioX, ratioY);
+
+                int newWidth = (int)(ogWidth * ratio);
+                int newHeight = (int)(ogHeight * ratio);
+
+                Bitmap newImage = new Bitmap(greyimg, newWidth, newHeight);
+                pb.SizeMode = PictureBoxSizeMode.CenterImage;
+                pb.Image = newImage;
+
+            };
+            /*
+            count = 0;
+            timer1.Tick += new EventHandler(printOneByOne);
+            timer1.Interval = 200;
+            textholder = "...Sure";
+            timer1.Start();
+            */
+            
+            
+        }
+        private void printOneByOne(Object o, EventArgs e)
+        {
+            if (count < textholder.Length)
+            {
+                label1.Text += textholder[count];
+                count++;
+            }
+            else
+            {
+                timer1.Stop();
+                label1.Text = "";
+                Random rnd = new Random();
+                int randomX = rnd.Next(1, this.ClientSize.Width - 183);
+                int randomY = rnd.Next(1, this.ClientSize.Height - 32);
+                this.button1.Text = randomX.ToString() + " , " + randomY.ToString();
+                //this.textBox1.SetBounds(randomX, randomY, 183, 32);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "JPG Files|*.jpg";
+            saveFileDialog1.FilterIndex = 0;
+            saveFileDialog1.DefaultExt = ".jpg";
+            saveFileDialog1.OverwritePrompt = false;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                greyimg.Save(saveFileDialog1.FileName);
+            }
+        }
+
+    }
+}
